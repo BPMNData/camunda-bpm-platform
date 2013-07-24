7ffe4ffe1b9dc58e6ebc2b5e770d7d294c829aa3
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2013-07-24 11:51:49 +0200

    feat(example): add correlation demo process applications

---
+++ examples/bpmn-data/correlation-demo-seller/src/main/java/de/hpi/uni/potsdam/bpmn_to_sql/SellerProcessApplication.java
@@ -0,0 +1,9 @@
+package de.hpi.uni.potsdam.bpmn_to_sql;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("seller")
+public class SellerProcessApplication extends ServletProcessApplication {
+
+}

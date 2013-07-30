6e98e99345a3fc60fff0467392647f1950b67569
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2013-07-30 15:09:03 +0200

    feat(bpmn-data): add update statement representation

--- engine/src/test/java/de/hpi/uni/potsdam/test/bpmn_to_sql/pattern/sql/SqlStatementTest.java
+++ engine/src/test/java/de/hpi/uni/potsdam/test/bpmn_to_sql/pattern/sql/SqlStatementTest.java
@@ -4,11 +4,14 @@ import junit.framework.TestCase;
 
 import org.junit.Assert;
 
+import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.AttributeSetClause;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.FromClause;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.InsertStatement;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.PlainValueWhereSubClause;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.SelectClause;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.SelectStatement;
+import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.SetClause;
+import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.UpdateStatement;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.ValuesClause;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.ValuesSubClause;
 import de.hpi.uni.potsdam.bpmn_to_sql.pattern.sql.WhereClause;
@@ -47,6 +50,27 @@ public class SqlStatementTest extends TestCase {
     values.addValuesSubClause(value1);
     values.addValuesSubClause(value2);
     statement.setValues(values);
+    
+    String sql = statement.toSqlString();
+    
+    Assert.assertEquals(expectedSql, sql);
+  }
+  
+  public void testUpdateStatementCreation() {
+    String expectedSql = "UPDATE a SET b = \"d\", c = \"e\" WHERE f = \"g\"";
+    
+    UpdateStatement statement = new UpdateStatement("a");
+    SetClause set = new SetClause();
+    AttributeSetClause attributeClause1 = new AttributeSetClause("b", "\"d\"");
+    AttributeSetClause attributeClause2 = new AttributeSetClause("c", "\"e\"");
+    set.addAttributeClause(attributeClause1);
+    set.addAttributeClause(attributeClause2);
+    statement.setSetClause(set);
+    
+    WhereClause where = new WhereClause();
+    WhereSubClause sub = new PlainValueWhereSubClause("f", "\"g\"");
+    where.addSubClause(sub);
+    statement.setWhereClause(where);
     
     String sql = statement.toSqlString();
     

b96d81cea3bb17060004d8fd30a3371947a9580b
dfahland <d.fahland@tue.nl>
2013-10-22 21:26:39 +0200

    + added project for evaluating BPMNData on service interaction patterns

---
+++ examples/interaction-patterns/patterns-partner-A/src/main/java/nl/tue/ais/bpmndata/choreographies/PatternEvaluation.java
@@ -0,0 +1,9 @@
+package nl.tue.ais.bpmndata.choreographies;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Pattern Evaluation - Partner A")
+public class PatternEvaluation extends ServletProcessApplication {
+
+}
